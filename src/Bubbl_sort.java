eafe9842e31e5c82490a3dd0ac9d738e918aa82d
qiandoulu <xiechunhao99@qq.com>
2020-09-23 20:08:49 +0800

    冒泡排序（排序student的成绩）

--- src/Bubbl_sort.java
+++ src/Bubbl_sort.java
@@ -31,14 +31,6 @@ public class Bubbl_sort {
     public static ArrayList<student> readFileByLines(String fileName) {
         File file = new File(fileName);
         ArrayList<student> stu=new ArrayList<student>();
-        /*静态的方法中不能直接调用非静态的方法或属性。
-        因为一个类的静态方法在这个Class文件被加载之后，
-        就可以由这个Class类型对象来调用，
-        而非静态的方法需要一个实例对象，
-        有可能还未被创建，所以为了避免在静态方法中调用一个还不存在的实例对象的非静态方法，
-        编译期就会直接阻止这个行为*/
-        Bubbl_sort bubble_sort=new Bubbl_sort();
-        student temp = bubble_sort.new student();
 
         BufferedReader reader = null;
         try {
@@ -51,14 +43,20 @@ public class Bubbl_sort {
                 //存入student链表
                 String[] str=tempString.split(" ");
                 //System.out.println(str[0]+" "+str[1]);
+                /*静态的方法中不能直接调用非静态的方法或属性。
+        因为一个类的静态方法在这个Class文件被加载之后，
+        就可以由这个Class类型对象来调用，
+        而非静态的方法需要一个实例对象，
+        有可能还未被创建，所以为了避免在静态方法中调用一个还不存在的实例对象的非静态方法，
+        编译期就会直接阻止这个行为*/
+                Bubbl_sort bubble_sort=new Bubbl_sort();
+                student temp = bubble_sort.new student();
                 temp.name=str[0].toString();
                 temp.grade=Integer.parseInt(str[1]);
                 //System.out.println(temp.name);
                 stu.add(temp);
             }
-            for(int i =0;i<stu.size();i++){
-                System.out.println(stu.get(i).name);
-            }
+
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -75,15 +73,16 @@ public class Bubbl_sort {
     public static void main(String args[]){
         ArrayList<student> stu_arr=new ArrayList<student>();
         stu_arr=readFileByLines("grade");//读取文件，传参
-
+//
 //        for(int i =0;i<stu_arr.size();i++){
 //            System.out.println(stu_arr.get(i).name);
 //        }
         student[] stu=new student[stu_arr.size()];
         for(int i=0;i<stu_arr.size();i++){
-            stu[i].name=stu_arr.get(i+1).name;
-            stu[i].grade=stu_arr.get(i+1).grade;
-
+//            System.out.println(stu_arr.get(i).name+":"+stu_arr.get(i).grade);
+//            stu[i].name=stu_arr.get(i).name;
+//            stu[i].grade=stu_arr.get(i).grade;wrong
+            stu[i]=stu_arr.get(i);
         }
         //排序
         Bubblesort(stu);
